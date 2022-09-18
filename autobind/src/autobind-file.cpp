55f941f0c83d01691f546801d2b49d47fd9c6dbd
Per Ottosson (ext) <otpe20pu@student.ju.se>
2022-09-18 02:48:01 +0200

    adds member m_structSpecifications

--- autobind/src/autobind-file.cpp
+++ autobind/src/autobind-file.cpp
@@ -10,14 +10,14 @@ static bool elementNameEquals(const tinyxml2::XMLElement *element, const std::st
 	return std::string(element->Value()).compare(string) == 0;
 }
 
-static FunctionSpec::Parameter parseParameterElement(const tinyxml2::XMLElement *parameterElement) {
+static Parameter parseParameterElement(const tinyxml2::XMLElement *parameterElement) {
 	auto nameAttribute = parameterElement->FindAttribute("name");
 	
-	std::unordered_map<std::string, FunctionSpec::Parameter::Type> parameterTypes;
+	std::unordered_map<std::string, Parameter::Type> parameterTypes;
 
-	parameterTypes["number"] = FunctionSpec::Parameter::Type::Number;
-	parameterTypes["string"] = FunctionSpec::Parameter::Type::String;
-	parameterTypes["userdata"] = FunctionSpec::Parameter::Type::UserData;
+	parameterTypes["number"] = Parameter::Type::Number;
+	parameterTypes["string"] = Parameter::Type::String;
+	parameterTypes["userdata"] = Parameter::Type::UserData;
 
 
 	if (!nameAttribute) {
@@ -31,11 +31,11 @@ static FunctionSpec::Parameter parseParameterElement(const tinyxml2::XMLElement 
 		throw "parameter missing type";
 	}
 
-	return FunctionSpec::Parameter(name, parameterTypes[typeAttribute->Value()]);
+	return Parameter(name, parameterTypes[typeAttribute->Value()]);
 }
 
-static std::vector<FunctionSpec::Parameter> parseParameterElements(const tinyxml2::XMLElement *root) {
-	std::vector<FunctionSpec::Parameter> parameters;
+static std::vector<Parameter> parseParameterElements(const tinyxml2::XMLElement *root) {
+	std::vector<Parameter> parameters;
 
 	const tinyxml2::XMLElement *parameterElement = root->FirstChildElement("Parameter");
 
@@ -54,27 +54,27 @@ static FunctionSpec parseFunctionElement(const tinyxml2::XMLElement *functionEle
 	}
 
 	const char *name = functionElement->FindAttribute("name")->Value();
-	std::vector<FunctionSpec::Parameter> parametersIn, parametersOut;
+	std::vector<Parameter> parametersIn, parametersOut;
 	const tinyxml2::XMLElement *element = 0;
 
 	// Parse in elements
 	// TODO: find a way around this code repetition 
 	element = functionElement->FirstChildElement("In");
 	if (element) {
-		std::vector<FunctionSpec::Parameter> params = parseParameterElements(element);
+		std::vector<Parameter> params = parseParameterElements(element);
 		std::copy(params.begin(), params.end(), std::back_inserter(parametersIn));
 	}
 
 	// Parse out elements
 	element = functionElement->FirstChildElement("Out");
 	if (element) {
-		std::vector<FunctionSpec::Parameter> params = parseParameterElements(element);
+		std::vector<Parameter> params = parseParameterElements(element);
 		std::copy(params.begin(), params.end(), std::back_inserter(parametersOut));
 	}
 
 	//Check for same name parameters - Illegal!
 	bool doubleParams = false;
-	for (const FunctionSpec::Parameter &pIn : parametersIn) {
+	for (const Parameter &pIn : parametersIn) {
 		for (auto pOut : parametersOut) {
 			if (pIn.name == pOut.name) {
 				doubleParams = true;
@@ -82,7 +82,7 @@ static FunctionSpec parseFunctionElement(const tinyxml2::XMLElement *functionEle
 			}
 		}
 
-		for (const FunctionSpec::Parameter &pIn2 : parametersIn) {
+		for (const Parameter &pIn2 : parametersIn) {
 			if (pIn.name == pIn2.name && &pIn != &pIn2) {
 				doubleParams = true;
 				break;
@@ -133,6 +133,6 @@ AutoBindFile AutoBindFile::LoadFromXML(std::string filePath) {
 		element = element->NextSiblingElement();
 	}
 
-	return AutoBindFile(moduleNameAttribute->Value(), functionSpecifications);
+	return AutoBindFile(moduleNameAttribute->Value(), functionSpecifications, std::vector<StructSpec>());
 
 }
--- autobind/src/autobind-file.h
+++ autobind/src/autobind-file.h
@@ -2,16 +2,23 @@
 #define AUTOBIND_FILE_H
 #include <vector>
 #include "function-spec.h"
+#include "struct-spec.hpp"
 
 class AutoBindFile {
 
 private:
 	const std::vector<FunctionSpec> m_functionSpecifications;
+	const std::vector<StructSpec> m_structSpecifications;
 	const std::string m_moduleName;
 
 public:
-	AutoBindFile(std::string moduleName, std::vector<FunctionSpec>& functionSpecifications)
-		: m_moduleName(moduleName), m_functionSpecifications(functionSpecifications) {}
+	AutoBindFile(
+		std::string moduleName, 
+		const std::vector<FunctionSpec> &functionSpecifications, 
+		const std::vector<StructSpec> &structSpecifications
+	) : m_moduleName(moduleName),
+		m_functionSpecifications(functionSpecifications),
+		m_structSpecifications(structSpecifications) {}
 
 	inline const std::vector<FunctionSpec> &getFunctionSpecifications() const {
 		return m_functionSpecifications;
