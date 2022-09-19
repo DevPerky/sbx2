743d1ba6c5239c3f7cf20b48c94d7539a8a9ae3f
Per Ottosson (ext) <otpe20pu@student.ju.se>
2022-09-19 22:37:11 +0200

    autogenerated changes

--- autobind/testOut/AB-test-implementation.c
+++ autobind/testOut/AB-test-implementation.c
@@ -1,103 +1,23 @@
 #include "AB-test-interface.h"
 #include <lauxlib.h>
 
-static AB_threeInThreeOut AB_threeInThreeOut_binding;
-void AB_bind_threeInThreeOut(AB_threeInThreeOut function) {
-	AB_threeInThreeOut_binding = function;
-}
-
-static AB_theeInZeroOut AB_theeInZeroOut_binding;
-void AB_bind_theeInZeroOut(AB_theeInZeroOut function) {
-	AB_theeInZeroOut_binding = function;
-}
-
-static AB_zeroInThreeOut AB_zeroInThreeOut_binding;
-void AB_bind_zeroInThreeOut(AB_zeroInThreeOut function) {
-	AB_zeroInThreeOut_binding = function;
+static AB_customTypeIn AB_customTypeIn_binding;
+void AB_bind_customTypeIn(AB_customTypeIn function) {
+	AB_customTypeIn_binding = function;
 }
 
 
-static int l_threeInThreeOut(lua_State *L) {
-	double in_param1;
-	const char *in_param2;
-	void *in_param3;
+static int l_customTypeIn(lua_State *L) {
+	Test customParam;
 
-	double out_param;
-	const char *out_param1;
-	void *out_param2;
 
-	if (lua_isuserdata(L, -1)) {
-		in_param3 = lua_touserdata(L, -1);
-	}
-	else {
-		return luaL_error(L, "Incorrect type for parameter in_param3. Expected type was userdata.");
-	}
-
-	if (lua_isstring(L, -2)) {
-		in_param2 = lua_tostring(L, -2);
-	}
-	else {
-		return luaL_error(L, "Incorrect type for parameter in_param2. Expected type was string.");
-	}
-
-	if (lua_isnumber(L, -3)) {
-		in_param1 = lua_tonumber(L, -3);
-	}
-	else {
-		return luaL_error(L, "Incorrect type for parameter in_param1. Expected type was number.");
-	}
-
-	if (AB_threeInThreeOut_binding) {
-		if (!AB_threeInThreeOut_binding(in_param1, in_param2, in_param3, &out_param, &out_param1, &out_param2)) {
-			return luaL_error(L, "Runtime error: threeInThreeOut failed for unknown reason!");
+	if (AB_customTypeIn_binding) {
+		if (!AB_customTypeIn_binding(customParam)) {
+			return luaL_error(L, "Runtime error: customTypeIn failed for unknown reason!");
 		}
 	}
 	else {
-		return luaL_error(L, "Runtime error: threeInThreeOut wasn't bound!");
-	}
-
-	lua_pushnumber(L, out_param);
-	lua_pushstring(L, out_param1);
-	lua_pushlightuserdata(L, out_param2);
-
-	return 3;
-}
-
-
-static int l_theeInZeroOut(lua_State *L) {
-	double out_param;
-	const char *out_param1;
-	void *out_param2;
-
-
-	if (lua_isuserdata(L, -1)) {
-		out_param2 = lua_touserdata(L, -1);
-	}
-	else {
-		return luaL_error(L, "Incorrect type for parameter out_param2. Expected type was userdata.");
-	}
-
-	if (lua_isstring(L, -2)) {
-		out_param1 = lua_tostring(L, -2);
-	}
-	else {
-		return luaL_error(L, "Incorrect type for parameter out_param1. Expected type was string.");
-	}
-
-	if (lua_isnumber(L, -3)) {
-		out_param = lua_tonumber(L, -3);
-	}
-	else {
-		return luaL_error(L, "Incorrect type for parameter out_param. Expected type was number.");
-	}
-
-	if (AB_theeInZeroOut_binding) {
-		if (!AB_theeInZeroOut_binding(out_param, out_param1, out_param2)) {
-			return luaL_error(L, "Runtime error: theeInZeroOut failed for unknown reason!");
-		}
-	}
-	else {
-		return luaL_error(L, "Runtime error: theeInZeroOut wasn't bound!");
+		return luaL_error(L, "Runtime error: customTypeIn wasn't bound!");
 	}
 
 
@@ -105,31 +25,6 @@ static int l_theeInZeroOut(lua_State *L) {
 }
 
 
-static int l_zeroInThreeOut(lua_State *L) {
-
-	double out_param;
-	const char *out_param1;
-	void *out_param2;
-
-	if (AB_zeroInThreeOut_binding) {
-		if (!AB_zeroInThreeOut_binding(&out_param, &out_param1, &out_param2)) {
-			return luaL_error(L, "Runtime error: zeroInThreeOut failed for unknown reason!");
-		}
-	}
-	else {
-		return luaL_error(L, "Runtime error: zeroInThreeOut wasn't bound!");
-	}
-
-	lua_pushnumber(L, out_param);
-	lua_pushstring(L, out_param1);
-	lua_pushlightuserdata(L, out_param2);
-
-	return 3;
-}
-
-
 void AB_registerModule_test(lua_State *L) {
-	lua_register(L, "threeInThreeOut", l_threeInThreeOut);
-	lua_register(L, "theeInZeroOut", l_theeInZeroOut);
-	lua_register(L, "zeroInThreeOut", l_zeroInThreeOut);
+	lua_register(L, "customTypeIn", l_customTypeIn);
 }
--- autobind/testOut/AB-test-interface.h
+++ autobind/testOut/AB-test-interface.h
@@ -1,10 +1,17 @@
 #include <lua.h>
 void AB_registerModule_test(lua_State *L);
-typedef int (*AB_threeInThreeOut)(double in_param1, const char *in_param2, void *in_param3, double *out_param, const char **out_param1, void **out_param2);
-typedef int (*AB_theeInZeroOut)(double out_param, const char *out_param1, void *out_param2);
-typedef int (*AB_zeroInThreeOut)(double *out_param, const char **out_param1, void **out_param2);
+typedef struct Test {
+	double testNumber;
+	const char *testString;
+	void *testUserData;
+}Test;
 
-void AB_bind_threeInThreeOut(AB_threeInThreeOut function);
-void AB_bind_theeInZeroOut(AB_theeInZeroOut function);
-void AB_bind_zeroInThreeOut(AB_zeroInThreeOut function);
+typedef struct Test2 {
+	Test customParam;
+}Test2;
+
+
+typedef int (*AB_customTypeIn)(Test customParam);
+
+void AB_bind_customTypeIn(AB_customTypeIn function);
 
