96cdbc532205b9c37ccde39a06e03add74d630db
Per Ottosson (ext) <otpe20pu@student.ju.se>
2022-10-04 08:19:57 +0200

    Rebase with master. Support for integers have been added and include guards are now generated by autobind.

--- src/modules/draw/AB-Draw-interface.h
+++ src/modules/draw/AB-Draw-interface.h
@@ -2,12 +2,30 @@
 #define AB_DRAW_H
 
 #include <lua.h>
+typedef struct Vector2 {
+		double x;
+		double y;
+}Vector2;
+
+typedef struct Rectangle {
+		double left;
+		double top;
+		double right;
+		double bottom;
+}Rectangle;
+
+typedef struct Camera {
+		Vector2 position;
+		Rectangle viewPort;
+}Camera;
 
 void AB_registerModule_Draw(lua_State *L);
+typedef int (*AB_SetDrawCamera)(Camera camera);
 typedef int (*AB_SetDrawColor)(double red, double green, double blue, double alpha);
 typedef int (*AB_DrawRectangle)(double left, double top, double right, double bottom);
 typedef int (*AB_DrawLine)(double fromX, double fromY, double toX, double toY, double width);
 typedef int (*AB_Clear)(double red, double green, double blue, double alpha);
+void AB_bind_SetDrawCamera(AB_SetDrawCamera function);
 void AB_bind_SetDrawColor(AB_SetDrawColor function);
 void AB_bind_DrawRectangle(AB_DrawRectangle function);
 void AB_bind_DrawLine(AB_DrawLine function);
--- test.lua
+++ test.lua
@@ -3,8 +3,21 @@
 
 local window = nil
 
+---@type Camera
+local camera = {
+	position = { x=0, y=0},
+	viewPort = { top = 0, left = 0, right = 800, bottom = 600 }
+}
+
+function OnWindowResize(width, height)
+	camera.viewPort.right = width
+	camera.viewPort.bottom = height
+	SetDrawCamera(camera)
+end
+
 function Init()
-	window = CreateWindow(800, 600, "test")
+	window = CreateWindow(camera.viewPort.right, camera.viewPort.bottom, "test")
+	SetDrawCamera(camera)
 	SetTargetFPS(90)
 end
 
@@ -12,7 +25,7 @@ function Update()
 	Clear(0.1, 0.1, 0.1, 1.0)
 	local seconds = GetSeconds()
 	local x, y = GetMouseXY(window)
-	local w, h = GetWindowDimensions(window);
+	local w, h = camera.viewPort.right, camera.viewPort.bottom
 	
 	SetDrawColor(0, 0, 1, 1)
 	DrawLine(0, 0, x, y, 10);
