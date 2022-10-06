b2ddeeeecfb98086132375ab0009aefcae58b699
otpe20pu <81689565+otpe20pu@users.noreply.github.com>
2022-10-06 11:10:56 +0200

    Renamed some functions to be more consistent

--- src/modules/draw/AB-Draw-interface.h
+++ src/modules/draw/AB-Draw-interface.h
@@ -21,20 +21,20 @@ typedef struct Camera {
 }Camera;
 
 void AB_registerModule_Draw(lua_State *L);
-typedef int (*AB_SetViewport)(Rectangle bounds);
-typedef int (*AB_CreateDrawMatrix)(lua_Integer *handle);
-typedef int (*AB_SetOrtho)(lua_Integer matrixHandle, Rectangle bounds);
-typedef int (*AB_SetProjectionMatrix)(lua_Integer matrixHandle);
-typedef int (*AB_SetDrawColor)(double red, double green, double blue, double alpha);
+typedef int (*AB_MatrixNew)(lua_Integer *handle);
+typedef int (*AB_MatrixSetOrtho)(lua_Integer matrixHandle, Rectangle bounds);
+typedef int (*AB_DrawSetViewport)(Rectangle bounds);
+typedef int (*AB_DrawSetProjectionMatrix)(lua_Integer matrixHandle);
+typedef int (*AB_DrawSetColor)(double red, double green, double blue, double alpha);
 typedef int (*AB_DrawRectangle)(double left, double top, double right, double bottom);
 typedef int (*AB_DrawLine)(double fromX, double fromY, double toX, double toY, double width);
-typedef int (*AB_Clear)(double red, double green, double blue, double alpha);
-void AB_bind_SetViewport(AB_SetViewport function);
-void AB_bind_CreateDrawMatrix(AB_CreateDrawMatrix function);
-void AB_bind_SetOrtho(AB_SetOrtho function);
-void AB_bind_SetProjectionMatrix(AB_SetProjectionMatrix function);
-void AB_bind_SetDrawColor(AB_SetDrawColor function);
+typedef int (*AB_DrawClear)(double red, double green, double blue, double alpha);
+void AB_bind_MatrixNew(AB_MatrixNew function);
+void AB_bind_MatrixSetOrtho(AB_MatrixSetOrtho function);
+void AB_bind_DrawSetViewport(AB_DrawSetViewport function);
+void AB_bind_DrawSetProjectionMatrix(AB_DrawSetProjectionMatrix function);
+void AB_bind_DrawSetColor(AB_DrawSetColor function);
 void AB_bind_DrawRectangle(AB_DrawRectangle function);
 void AB_bind_DrawLine(AB_DrawLine function);
-void AB_bind_Clear(AB_Clear function);
+void AB_bind_DrawClear(AB_DrawClear function);
 #endif
--- test.lua
+++ test.lua
@@ -15,30 +15,29 @@ local projectionMatrix = 0
 function OnWindowResize(width, height)
 	camera.viewPort.right = width
 	camera.viewPort.bottom = height
-	SetOrtho(projectionMatrix, camera.viewPort)
-	SetViewport(camera.viewPort)
-
-	SetProjectionMatrix(projectionMatrix)
+	MatrixSetOrtho(projectionMatrix, camera.viewPort)
+	DrawSetViewport(camera.viewPort)
+	DrawSetProjectionMatrix(projectionMatrix)
 end
 
 function Init()
 	SetTargetFPS(90)
 	window = CreateWindow(camera.viewPort.right, camera.viewPort.bottom, "test")
-	projectionMatrix = CreateDrawMatrix()
+	projectionMatrix = MatrixNew()
 	OnWindowResize(camera.viewPort.right, camera.viewPort.bottom)
 	print(projectionMatrix)
 end
 
 function Update()
-	Clear(0.1, 0.1, 0.1, 1.0)
+	DrawClear(0.1, 0.1, 0.1, 1.0)
 	local seconds = GetSeconds()
 	local x, y = GetMouseXY(window)
 	local w, h = camera.viewPort.right, camera.viewPort.bottom
 	
-	SetDrawColor(0, 0, 1, 1)
+	DrawSetColor(0, 0, 1, 1)
 	DrawLine(0, 0, x, y, 10);
-	SetDrawColor(1, 0, 0, 1);
+	DrawSetColor(1, 0, 0, 1);
 	DrawRectangle(0, h-20, (seconds/60)*w, h)
-	SetDrawColor(0, 1, 0, 1);
+	DrawSetColor(0, 1, 0, 1);
 	DrawRectangle(x - 5, y - 5, x + 5, y + 5)
 end
